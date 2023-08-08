69ce5d92c613b86568142b51fe00fbecb7a2b206
Giulio Perda <giulioperda@gmail.com>
2023-08-08 09:41:22 +0200

    Uploaded vostok_prepare.py
    Extract VOSTOK meaningful columns from ASCII point cloud x,y,z,nx,ny,nz, removing color columns

---
+++ vostok_prepare.py
@@ -0,0 +1,13 @@
+# Extract VOSTOK meaningful columns from ASCII point cloud x,y,z,nx,ny,nz, removing color columns
+
+import pandas as pd
+
+# Load the CSV file into a DataFrame
+path = r"C:\Users\admin\Desktop\GRAZ\metashape\graz_dense_medium_subsample.csv"
+df = pd.read_csv(path)
+
+# Extract the x,y,z,nx,ny,nz columns
+new_df = df.iloc[:, [0,1,2,6,7,8]]
+
+# Save the extracted columns to a new CSV file
+new_df.to_csv('r"C:\Users\admin\Desktop\GRAZ\metashape\vostok_input.csv"', index=False)
