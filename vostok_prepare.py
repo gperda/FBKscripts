9b90248edd8530ef16db3265b3968add6b7d57df
Giulio Perda <giulioperda@gmail.com>
2023-08-10 09:45:44 +0200

    Updated vostok_prepare.py
    Added separator when reading csv

--- vostok_prepare.py
+++ vostok_prepare.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # Load the CSV file into a DataFrame
 path = r"C:\Users\admin\Desktop\GRAZ\VOSTOK\graz_dense_medium_subsample.csv"
-df = pd.read_csv(path, float_precision='round_trip')
+df = pd.read_csv(path, sep=' ', float_precision='round_trip')
 
 # Extract the x,y,z,nx,ny,nz columns
 new_df = df.iloc[:, [0,1,2,6,7,8]]
