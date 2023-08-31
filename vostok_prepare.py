b3fb4d92dd05810fa1e0153dbb957fc5c3e8466c
Giulio Perda <giulioperda@gmail.com>
2023-08-31 17:48:43 +0200

    Updated vostok_prepare.py with input params

--- vostok_prepare.py
+++ vostok_prepare.py
@@ -1,13 +1,18 @@
 # Extract VOSTOK meaningful columns from ASCII point cloud x,y,z,nx,ny,nz, removing color columns
-
 import pandas as pd
+import sys
 
 # Load the CSV file into a DataFrame
-path = r"C:\Users\admin\Desktop\GRAZ\VOSTOK\graz_dense_medium_subsample.csv"
-df = pd.read_csv(path, sep=' ', float_precision='round_trip')
+
+input = sys.argv[1]
+output = input.replace('csv','xyz')
+print('Importing file ', input)
+df = pd.read_csv(input, sep =' ', float_precision='round_trip')
 
 # Extract the x,y,z,nx,ny,nz columns
 new_df = df.iloc[:, [0,1,2,6,7,8]]
 
 # Save the extracted columns to a new CSV file
-new_df.to_csv(r'C:\Users\admin\Desktop\GRAZ\VOSTOK\vostok_input.xyz',sep=' ', index=False)
+new_df.to_csv(output,sep=' ', index=False)
+
+print('Script finished. Output written to ', output)
